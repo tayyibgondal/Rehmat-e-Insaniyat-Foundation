a2900533792e06a8e571ec32d5f672d41ea1a0d7
TaimoorIkram <tikram.bese21seecs@seecs.edu.pk>
2022-12-26 19:33:07 +0500

    More UI changes.
    UI changes.
    - box style changed from sharp rounds to lenient rounds to give firmer look.
    - added slideshow menu at the home page
    - other quality of life changes.

--- static/javascript/sticky_header.js
+++ static/javascript/sticky_header.js
@@ -1,16 +1,23 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
+
 // Get the header
 var header = document.getElementById("pg_subheader");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
+if(document.getElementById('main_home') != undefined) {
+  header.classList.add('transparent_bg');
+}
+
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    header.classList.add("main_hd_solid_bg");
   } else {
     header.classList.remove("sticky");
+    header.classList.remove("main_hd_solid_bg");
   }
 }
